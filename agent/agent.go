7723c072e0d9c66396821d826a1a517e7d167937
Javier Teso <jtejob@gmail.com>
2015-08-13 08:42:28 +1000

    Godeps, Docker setup

--- agent/agent.go
+++ agent/agent.go
@@ -1,59 +1,47 @@
 package agent
 
-import(
+import (
 	"fmt"
+	"github.com/jteso/xchronos/Godeps/_workspace/src/github.com/coreos/go-etcd/etcd"
 	"time"
 )
 
-type Operation interface{
-	GetAckCh() chan bool
-	WaitForAck()
-}
-
-type TerminateOp struct {
-	ack chan bool
-}
-func (this TerminateOp) GetAckCh() chan bool {
-	return this.ack
-}
-func (this TerminateOp) WaitForAck() {
-	<- this.ack
-}
-
-func NewTerminateOp() Operation {
-	return &TerminateOp{
-		ack: make(chan bool),
-	}
-}
-
-type AckableOpCh chan Operation
-
+const (
+	SCHEDULER_ELECTION_KEY = "/xchronos/scheduler/election"
+	SCHEDULER_LEADER_TTL   = 5
+)
 
 type AgentState int
 
-const(
+const (
 	STATE_INIT AgentState = iota
 	STATE_ELECT_FOR_LEADER
 	STATE_NODE_BECOME_LEADER
 	STATE_NODE_BECOME_FOLLOWER
 	STATE_NODE_ERROR
 )
+
 // agentStateFn represents the state of an agent as a function
 // that returns the next state
 type handleStateFn func(*Agent) handleStateFn
 
 type Agent struct {
-	Name string
-	stopCh AckableOpCh
+	ID         string
+	etcdClient *etcd.Client
+	etcdNodes  []string
 }
-func New(name string, stopCh AckableOpCh) *Agent {
+
+// etcdNotes = strings.Split(os.Getenv("ETCD_NOTES"), ",")
+func New(id string, etcdNodes []string) *Agent {
 	return &Agent{
-		Name: name,
-		stopCh: stopCh,
+		ID:        id,
+		etcdNodes: etcdNodes,
 	}
 }
 
-func (a *Agent) errorf(format string, args ...interface{}) handleStateFn {
+// panic function will force an agent to stop its execution, due to some
+// unrecoverable error found.
+func (a *Agent) panic(format string, args ...interface{}) handleStateFn {
 	fmt.Printf(format, args)
 	return nil
 }
@@ -62,7 +50,7 @@ func (a *Agent) Run() error {
 	for stateHandler := initAgent; stateHandler != nil; {
 		stateHandler = stateHandler(a)
 	}
-	close(a.stopCh)
+	//close(a.stopCh)
 	return nil
 }
 
@@ -71,16 +59,50 @@ func initAgent(agent *Agent) handleStateFn {
 }
 
 func joinEtcdCluster(agent *Agent) handleStateFn {
-	fmt.Printf("Agent: %s joining etcd cluster...\n", agent.Name)
+	fmt.Printf("[Agent:%s] Joining etcd cluster...\n", agent.ID)
+	agent.etcdClient = etcd.NewClient(agent.etcdNodes)
+
 	time.Sleep(2 * time.Second)
-	return electForLeader
+	return runningForLeader
 }
 
-func electForLeader(agent *Agent) handleStateFn {
-	fmt.Printf("Agent: %s electing for leader\n", agent.Name)
-	time.Sleep(2 * time.Second)
+// TODO - use the Atomic Compare and Swap (CAS) for the locking service
+func runningForLeader(agent *Agent) handleStateFn {
+	fmt.Printf("[Agent:%s] Running for leader...\n", agent.ID)
+
+	// Put value if prevExist=false
+	_, err := agent.etcdClient.Create(SCHEDULER_ELECTION_KEY, agent.ID, SCHEDULER_LEADER_TTL)
+
+	if err != nil {
+		if etcdError, ok := err.(*etcd.EtcdError); ok {
+			// key already exists
+			if etcdError.ErrorCode == 105 {
+				return actAsSupporter
+			}else{
+				return agent.panic("Etcd returned unexpected error: %s", etcdError.Error())
+			}
+		}else{
+			return agent.panic("Error while electing for leadership due to: %s", err.Error())
+		}
+		
+	}
+
+	return actAsLeader
+}
+
+func actAsLeader(agent *Agent) handleStateFn {
+	fmt.Printf("[Agent:%s] stepping up as leader\n", agent.ID)
+	// ensure periodic leadership renewal
+	// setup crontab in mem on local node
+	// [cron signal] publish job offers
+	// [etcd leader watcher] return electForLeader
+	// [etcd jobOffer watcher] competeForJobExecution
 	return nil
 }
 
-
-
+func actAsSupporter(agent *Agent) handleStateFn {
+	fmt.Printf("[Agent:%s] stepping down\n", agent.ID)
+	// [etcd leader watcher] return electForLeader
+	// [etcd jobOffer watcher] competeForJobExecution
+	return nil
+}
--- main.go
+++ main.go
@@ -5,38 +5,36 @@ import(
 	"github.com/jteso/xchronos/agent"
 	"runtime"
 	"os"
+	"flag"
 	"time"
 	"fmt"
 )
 
+const(
+	FLAG_ETCD_NODES = "etcd-nodes"
+)
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	os.Exit(realMain())
 }
 
-// func realMain() int {
-// 	client := etcd.NewClient([]string{"http://127.0.0.1:4001"})
-// 	resp, err := client.Get("creds", false, false)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	log.Printf("Current creds: %s: %s\n", resp.Node.Key, resp.Node.Value)
-// 	watchChan := make(chan *etcd.Response)
-// 	go client.Watch("/creds", 0 , false, watchChan, nil)
-// 	log.Println("Waiting for an update...")
-// 	r := <- watchChan
-// 	log.Printf("Got updated creds: %s: %s \n", r.Node.Key, r.Node.Value)
-// 	return 0
-// }
+
+func parseFlags() map[string] string {
+	result := make(map[string] string)
+
+	etcdNodes := flag.String(FLAG_ETCD_NODES, "", "Comma separated list of etcd nodes")
+	flag.Parse()
+
+	result[FLAG_ETCD_NODES] = *etcdNodes
+	return result
+}
 
 func realMain() int{
+	flags := parseFlags()
 
-	a1_stopCh := make(agent.AckableOpCh)
-	a2_stopCh := make(agent.AckableOpCh)
-
-	a1 := agent.New("agent_1", a1_stopCh)
-	a2 := agent.New("agent_2", a2_stopCh)
+	a1 := agent.New("agent_1", []string{flags[FLAG_ETCD_NODES]})
+	a2 := agent.New("agent_2", []string{flags[FLAG_ETCD_NODES]})
 
 	
 	go a1.Run()
@@ -44,22 +42,6 @@ func realMain() int{
 	
 	time.Sleep(5 * time.Second)
 	
-	// fmt.Println("Sending stop signal to agents...")
-
-	// terminate_1 := agent.NewTerminateOp()
-	// terminate_2 := agent.NewTerminateOp()
-
-	// a1_stopCh <- terminate_1
-	// a2_stopCh <- terminate_2
-
-	
-	// fmt.Println("Waiting for agents to stop gracefully")	
-	
-
-	// terminate_1.WaitForAck()
-	// terminate_2.WaitForAck()
-
-
 	fmt.Println("System halted successfully :)")
 	return 0
 }
